a904203087504463554d789e52aeba30b56e788c
Astrrokid <kidstarro22@gmail.com>
2023-05-05 16:41:14 +0100

    first commit

---
+++ src/componenets/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Login=()=>{
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <div className="header">
+                    <h3>Create Account</h3>
+                </div>
+                <form action="">
+                    <div className="un">
+                        <label htmlFor="">Username</label>
+                        <input type="username" />
+                    </div>
+                    <div className="un">
+                        <label htmlFor="">Username</label>
+                        <input type="username" />
+                    </div>
+                    <div className="un">
+                        <label htmlFor="">Username</label>
+                        <input type="username" />
+                    </div>
+                    <div className="un">
+                        <label htmlFor="">Username</label>
+                        <input type="username" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
