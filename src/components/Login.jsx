a5be2ef0a5c9b6e5971cccb2cebf0d705f82e827
Astrrokid <kidstarro22@gmail.com>
2023-05-05 18:13:05 +0100

    third commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,25 +9,25 @@ const Login=()=>{
                 </div>
                 <form action="">
                     <div className="un">
-                        <label htmlFor="">Username</label>
+                        <label htmlFor="">Username</label> <br />
                         <input type="username" />
                     </div>
                     <div className="un">
-                        <label htmlFor="">Email Address</label>
+                        <label htmlFor="">Email Address</label> <br />
                         <input type="email" />
                     </div>
                     <div className="un">
-                        <label htmlFor="">Phone Number</label>
+                        <label htmlFor="">Phone Number</label> <br />
                         <input type="number" />
                     </div>
                     <div className="un">
-                        <label htmlFor="">Password</label>
-                        <input type="passsword" />
+                        <label htmlFor="">Password</label> <br />
+                        <input type="password" />
                     </div>
-                    <div className="checkbox">
-                        <input type="checkbox" />
+                    <div className="cb">
+                        <input type="checkbox" /> Remember Me
                     </div>
-                    <button>Submit</button>
+                    <div className="un"><button>Create Account</button></div>
                 </form>
             </div>
         </div>
